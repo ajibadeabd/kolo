a0ae89e0cbab6cadb0d5728c35dd3f786bde4222
ajibadeabd <ajibadeabd@gmail.com>
2020-11-25 20:56:34 +0100

    added

---
+++ precache-manifest.80144009a0010873785f04d798a98068.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5908df343fda5f68035",
+    "url": "/css/app.4c6291da.css"
+  },
+  {
+    "revision": "2368d2babe90e4278edca4f37ad9ae61",
+    "url": "/img/1.2368d2ba.png"
+  },
+  {
+    "revision": "4204f82f40e8a10a22aa33dffafd97c8",
+    "url": "/img/6.4204f82f.png"
+  },
+  {
+    "revision": "bd861cedb543725976fa6ecaaa191b31",
+    "url": "/img/7.bd861ced.png"
+  },
+  {
+    "revision": "0a07e2a1f4d23e70eb33304806cc05ea",
+    "url": "/img/9.0a07e2a1.png"
+  },
+  {
+    "revision": "210791973fa9af36822b9b865d74511c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5908df343fda5f68035",
+    "url": "/js/app.7231d8c9.js"
+  },
+  {
+    "revision": "ae0e715ec118370fa791",
+    "url": "/js/chunk-vendors.92014de6.js"
+  },
+  {
+    "revision": "567928969baf13c407cc2d705e213870",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- src/config/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default () => {
-    return axios.create({
-      // baseURL: "http://localhost:24434/v1/api",
-      baseURL: "https://kolo-cash.herokuapp.com/v1/api",
-      // baseURL: "/v1/api",
-    });
-}
